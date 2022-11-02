1dddb6feb52a73853d23207075f9e9c591e62444
K0dikas <kodikasYT@protonmail.com>
2022-11-02 16:26:00 +0800

    Changed the init to be optimized

--- init.lua
+++ init.lua
@@ -1,7 +1,15 @@
-vim.g.did_load_filetypes = 1
-vim.defer_fn(function()
-	pcall(require, "impatient")
-end, 0)
+_G.__luacache_config = {
+	chunks = {
+		enable = true,
+		path = vim.fn.stdpath('cache')..'/luacache_chunks',
+	},
+	modpaths = {
+		enable = true,
+		path = vim.fn.stdpath('cache')..'/luacache_modpaths',
+	}
+}
+require('impatient')
 
+vim.g.did_load_filetypes = 1
 require "core"
 require "core.init"
