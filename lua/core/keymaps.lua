e6e8535cf7a228f42630691a74b49106b3bb39ed
K0dikas <kodikasYT@protonmail.com>
2022-11-02 17:16:44 +0800

    Nothing really changed

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('i', '<C-g>', '<esc>')
 vim.keymap.set('n', '<C-b>', ':NeoTreeShow<cr>')
 vim.keymap.set('n', '<C-x>',
-  require("lsp_lines").toggle,
+require("lsp_lines").toggle,
   { desc = "Toggle lsp_lines" }
 )
