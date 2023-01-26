db77c649a747ce6d5edd1dbfcc2e6b2af3131c76
K0dikas <kodikasYT@protonmail.com>
2023-01-26 09:04:25 +0800

    Added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,22 @@
-vim.keymap.set('i', '<C-g>', '<esc>')
+local opts = {noremap = true, silent = true}
+
 vim.keymap.set('n', '<C-b>', ':NeoTreeShow<cr>')
-vim.keymap.set('n', '<C-x>',
-require("lsp_lines").toggle,
-  { desc = "Toggle lsp_lines" }
-)
+
+-- Move to the next buffer
+vim.keymap.set('n', '<C-k>', '<Cmd>BufferNext<cr>', opts)
+vim.keymap.set('n', '<C-j>', '<Cmd>BufferPrevious<cr>', opts)
+vim.keymap.set('n', '<C-x>', '<Cmd>BufferClose<cr>', opts)
+
+-- Activate Automaton animation
+vim.keymap.set('n', '<leader>rain', '<Cmd>CellularAutomaton make_it_rain<cr>', opts) -- Char fall
+vim.keymap.set('n', '<leader>fire', '<Cmd>CellularAutomaton game_of_life<cr>', opts) -- Fire
+
+-- Activate duck movement
+vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🦆", 13) end, {}) -- A pretty fast duck
+vim.keymap.set('n', '<leader>dc', function() require("duck").hatch("🐈", 5) end, {}) -- Quite a mellow cat
+
+-- Activate GPT-3 
+vim.keymap.set('n', '<leader>ll', '<Cmd>ChatGPT<cr>', opts)
+
+-- Activate side Panel
+vim.keymap.set("n", "<leader>oo", function() require("panel")(" ") end, {})
