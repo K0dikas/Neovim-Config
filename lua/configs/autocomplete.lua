50ce93969430eeba5b27c8b116ad9ffdc381469b
K0dikas <kodikasYT@protonmail.com>
2023-03-12 21:41:15 +0800

    Move two variables and removed a duplicated variable

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -2,12 +2,14 @@ local M = {}
 
 function M.config()
 
-	local cmp = require 'cmp'
+	local luasnip = require 'luasnip'
+    local cmp = require 'cmp'
+
 	cmp.setup({
 		snippet = {
             -- REQUIRED - you must specify a snippet engine
 			expand = function(args)
-                require('luasnip').lsp_expand(args.body)
+				luasnip.lsp_expand(args.body)
             end,
         },
 
@@ -47,9 +49,6 @@ function M.config()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
-
-	local luasnip = require("luasnip")
-    local cmp = require("cmp")
 
     cmp.setup({
 
