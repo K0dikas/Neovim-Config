1ec6baf9a525c867a725cd2f97e6f669684d0013
K0dikas <kodikasYT@protonmail.com>
2023-03-27 17:19:39 +0800

    Added Java, TypeScript, HTML, and css language servers

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.config()
 	require("mason-lspconfig").setup({
-		ensure_installed = {"lua_ls"}
+		ensure_installed = {"lua_ls", "jdtls", "tsserver", "html", "tailwindcss"}
 	})
 
 end
