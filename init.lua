cbfbe3084bdca91137881f0bb3521827e691e83c
K0dikas <kodikasYT@protonmail.com>
2023-04-10 18:53:16 +0800

    Added automatic installation of packer.nvim if user does not have packer installed

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+		vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+	end
+
 _G.__luacache_config = {
 	chunks = {
 		enable = true,
