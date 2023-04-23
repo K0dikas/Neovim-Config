5d50f11687aab4b6978ec6c88b4bc5c92d43d424
K0dikas <kodikasYT@protonmail.com>
2023-04-23 22:08:45 +0800

    added lsp servers

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.config()
 	require("mason-lspconfig").setup({
-		ensure_installed = {}
+		ensure_installed = {"lua_ls", "tsserver", "html", "tailwindcss", "rust-analyzer"}
 	})
 
 end
