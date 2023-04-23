4639946c58737e7f43ecba7dcb563dc813c0480e
K0dikas <kodikasYT@protonmail.com>
2023-04-23 20:59:20 +0800

    Moved all lsp to a different directory, probably not the best idea

---
+++ lua/lsp/tsserver.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.config()
+
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	local on_attach = on_attach
+
+	require("lsp_lines").setup{
+		require'lspconfig'.tsserver.setup{
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = {"javascript" ,"typescript", "typescriptreact", "typescript.tsx"},
+			cmd = {"typescript-language-server", "--stdio"}
+		}
+	}
+end
+return M
