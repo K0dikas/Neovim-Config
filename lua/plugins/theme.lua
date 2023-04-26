a39de176a4f33267bb574e815624652bcb43eb7d
K0dikas <kodikasYT@protonmail.com>
2023-04-26 16:14:05 +0800

    fixed theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,29 +1,29 @@
-local M = {
+return {
 	"ellisonleao/gruvbox.nvim",
 	event = "BufReadPre",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		undercurl = false,
-		underline = false,
-		bold = true,
-		italic = {
-			strings = false,
-			comments = false,
-			operators = false,
-			folds = false,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true, -- invert background for search, diffs, statuslinesand errors
-		contrast = "hard",  -- can be "hard", "soft" or empty string
-		overrides = {},
-	vim.cmd("set background=dark"),
-	--vim.cmd("colorscheme gruvbox"),
-	},
+	config = function()
+		require("gruvbox").setup({
+			undercurl = false,
+			underline = false,
+			bold = true,
+			italic = {
+				strings = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true, -- invert background for search, diffs, statuslinesand errors
+			contrast = "hard",  -- can be "hard", "soft" or empty string
+			overrides = {},
+			vim.cmd("colorscheme gruvbox"),
+			vim.cmd("set background=dark"),
+		})
+	end,
 }
-
-return M
