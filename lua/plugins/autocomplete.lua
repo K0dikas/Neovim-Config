c4befe5dde8743e8bbacc61969b3ee502dd16ba6
K0dikas <kodikasYT@protonmail.com>
2023-04-26 15:42:58 +0800

    Fixed layout of autocmp

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
 	"hrsh7th/nvim-cmp",
 	event = "BufReadPre",
 	dependencies = {
@@ -9,109 +9,119 @@ local M = {
 		"L3MON4D3/LuaSnip",
 		"onsails/lspkind.nvim",
 	},
-}
-
-opts = function()
-	local cmp = require('cmp')
-	local lsp_kinds = require("utils").lsp_kinds
-	local has_words_before = function()
-		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-			return false
+	config = function()
+		local cmp = require('cmp')
+		local lsp_kinds = require("utils").lsp_kinds
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = vim.F.unpack_len(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
-		local line, col = vim.F.unpack_len(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
-	local lspkind = require('lspkind')
-	local luasnip = require("luasnip")
+		local lspkind = require('lspkind')
+		local luasnip = require("luasnip")
 
-	return {
-		snippet = {
-			-- REQUIRED - you must specify a snippet engine
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
-
-		mapping = {
-			['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-			['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-			['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-			['<C-y>'] = cmp.config.disable,
-			['<C-e>'] = cmp.mapping({
-				i = cmp.mapping.abort(),
-				c = cmp.mapping.close(),
-			}),
-
-			['<CR>'] = cmp.mapping.confirm{
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true,
+		cmp.setup({
+			snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
 			},
 
+			mapping = {
+				['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+				['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+				['<C-y>'] = cmp.config.disable,
+				['<C-e>'] = cmp.mapping({
+					i = cmp.mapping.abort(),
+					c = cmp.mapping.close(),
+				}),
 
-			["<C-n>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				elseif has_words_before() then
-					cmp.complete()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+				['<CR>'] = cmp.mapping.confirm{
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				},
 
-			["<C-p>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
+
+				["<C-n>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+					end, { "i", "s" }),
+
+				["<C-p>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
 					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+					else
+						fallback()
+					end
+					end, { "i", "s" }),
 
-		},
+			},
 
-		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-		}, { { name = 'buffer' } }),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				}, { { name = 'buffer' } }),
 
-		formatting = {
-			format = lspkind.cmp_format({
-				mode = 'symbol_text',
-				maxwidth = 50,
-				ellipsis_char = '...',
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = 'symbol_text',
+					maxwidth = 50,
+					ellipsis_char = '...',
+				})
+			},
+		})
+
+		-- nvim-cmp for commands
+		cmp.setup.cmdline('/', {
+			sources = {
+				{ name = 'buffer' }
+			}
+		})
+		cmp.setup.cmdline(':', {
+			sources = cmp.config.sources({
+				{ name = 'path' }
+				}, {
+					{ name = 'cmdline' }
 			})
-		},
-	}
-end
+		})
 
-config = function(_, opts)
-	-- nvim-cmp for commands
-	cmp.setup.cmdline({'/', '?'}, {
-		sources = {
-			{ name = 'buffer' }
-		}
-	})
-	cmp.setup.cmdline(':', {
-		sources = cmp.config.sources({
-			{ name = 'path' }
-		}, {
-			{ name = 'cmdline' }
-		}),
-	})
-end
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+		--require "lsp.rust-analyzer".config()
+		--require("lsp.lua_ls").config()
+		--require("lsp.jdtls").config()
+		--require("lsp.html").config()
+		--require("lsp.tsserver").config()
+		--require("lsp.tailwindcss").config()
 
---require "lsp.rust-analyzer".config()
---require("lsp.lua_ls").config()
---require("lsp.jdtls").config()
---require("lsp.html").config()
---require("lsp.tsserver").config()
---require("lsp.tailwindcss").config()
-
-return M
+		local devicons = require('nvim-web-devicons')
+		cmp.register_source('devicons', {
+			complete = function(_, _, callback)
+				local items = {}
+				for _, icon in pairs(devicons.get_icons()) do
+					table.insert(items, {
+						label = icon.icon .. '  ' .. icon.name,
+						insertText = icon.icon,
+						filterText = icon.name,
+					})
+				end
+				callback({ items = items })
+			end,
+		})
+	end,
+}
