b6e27adf0d3a527375d67997fccff3b06c490e9b
K0dikas <kodikasYT@protonmail.com>
2023-04-26 16:44:42 +0800

    disabled performance mode

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,6 +1,6 @@
 return {
 	"karb94/neoscroll.nvim",
-	config = function ()
+	config = function()
 		require("neoscroll").setup{
 			mappings = {'<C-u>', '<C-d>', '<C-f>',
 				'<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
@@ -12,7 +12,7 @@ return {
 			easing_function = nil,       -- Default easing function
 			pre_hook = nil,              -- Function to run before the scrolling animation starts
 			post_hook = nil,             -- Function to run after the scrolling animation ends
-			performance_mode = true,
+			performance_mode = false,
 		}
 	end,
 }
