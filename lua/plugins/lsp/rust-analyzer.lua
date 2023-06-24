48a69fca98890a7d0ea55c043a3d6d06acef0eeb
K0dikas <kodikasYT@protonmail.com>
2023-06-24 17:06:23 +0800

    Added rust linter, Clippy

--- lua/plugins/lsp/rust-analyzer.lua
+++ lua/plugins/lsp/rust-analyzer.lua
@@ -1,23 +1,29 @@
 local M = {}
 
 function M.config()
-
 	local capabilities = require('cmp_nvim_lsp').default_capabilities()
 	local on_attach = on_attach
 
-	require("lsp_lines").setup{
+	require("lsp_lines").setup {
 
 		require('lspconfig')['rust_analyzer'].setup{
 			capabilities = capabilities,
 			on_attach = on_attach,
 			flags = lsp_flags,
 			settings = {
-				["rust-analyzer"] = {}
+				["rust-analyzer"] = {
+					checkOnSave = true,
+					check = {
+						enable = true,
+						command = "clippy",
+						features = "all",
+					},
+				},
 			}
 		}
 	}
 
-	require('rust-tools').setup()
 
 end
+	require('rust-tools').setup()
 return M
