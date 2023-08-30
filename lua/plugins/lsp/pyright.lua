d5b90bdd321e3941b85fe98fa79be6ef1ad8ea60
K0dikas <kodikasYT@protonmail.com>
2023-08-30 17:32:05 +0800

    Added mobile setup for python development

---
+++ lua/plugins/lsp/pyright.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.config()
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	local on_attach = on_attach
+
+	require("lsp_lines").setup {
+
+		require("lspconfig")['pyright'].setup {
+			capabilities = capabilities,
+			on_attach = on_attach,
+			flags = lsp_flags,
+			filetypes = {"python"},
+		}
+	}
+end
+return M
