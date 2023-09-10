7eeb1cc3e184878858f516ffde5ab024b6b97002
K0dikas <kodikasYT@protonmail.com>
2023-09-10 15:47:00 +0800

    fixed their indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,12 +9,12 @@ vim.opt.autowrite       = true
 vim.opt.showmode		= false
 vim.opt.cmdheight		= 0
 vim.diagnostic.config({
-	virtual_text = false,
-	virtual_lines = true,
+    virtual_text = false,
+    virtual_lines = true,
 })
 
 if (vim.fn.has('termguicolors') == 1) then
-	vim.opt.termguicolors = true
+    vim.opt.termguicolors = true
 end
 
 -- TABS
@@ -28,21 +28,21 @@ vim.opt.shiftwidth      = 4
 
 -- DISABLE SEMANTIC HIGHLIGHTING
 local links = {
-	['@lsp.type.namespace'] = '@namespace',
-	['@lsp.type.type'] = '@type',
-	['@lsp.type.class'] = '@type',
-	['@lsp.type.enum'] = '@type',
-	['@lsp.type.interface'] = '@type',
-	['@lsp.type.struct'] = '@structure',
-	['@lsp.type.parameter'] = '@parameter',
-	['@lsp.type.variable'] = '@variable',
-	['@lsp.type.property'] = '@property',
-	['@lsp.type.enumMember'] = '@constant',
-	['@lsp.type.function'] = '@function',
-	['@lsp.type.method'] = '@method',
-	['@lsp.type.macro'] = '@macro',
-	['@lsp.type.decorator'] = '@function',
+    ['@lsp.type.namespace'] = '@namespace',
+    ['@lsp.type.type'] = '@type',
+    ['@lsp.type.class'] = '@type',
+    ['@lsp.type.enum'] = '@type',
+    ['@lsp.type.interface'] = '@type',
+    ['@lsp.type.struct'] = '@structure',
+    ['@lsp.type.parameter'] = '@parameter',
+    ['@lsp.type.variable'] = '@variable',
+    ['@lsp.type.property'] = '@property',
+    ['@lsp.type.enumMember'] = '@constant',
+    ['@lsp.type.function'] = '@function',
+    ['@lsp.type.method'] = '@method',
+    ['@lsp.type.macro'] = '@macro',
+    ['@lsp.type.decorator'] = '@function',
 }
 for newgroup, oldgroup in pairs(links) do
-	vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+    vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
 end
