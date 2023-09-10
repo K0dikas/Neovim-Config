d9488178291736a0cd064d9a6b3b5276d0e2af02
K0dikas <kodikasYT@protonmail.com>
2023-09-10 15:34:54 +0800

    experimented a fix for indentation

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,29 +1,29 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	event = "VeryLazy",
-	lazy = false,
-	config = function()
-		require("gruvbox").setup({
-			undercurl = false,
-			underline = false,
-			bold = true,
-			italic = {
-				strings = false,
-				comments = false,
-				operators = false,
-				folds = false,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true, -- invert background for search, diffs, statuslinesand errors
-			contrast = "hard",  -- can be "hard", "soft" or empty string
-			overrides = {},
-			vim.cmd("set background=dark")
-		})
-			vim.cmd("colorscheme gruvbox")
-	end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    event = "VeryLazy",
+    lazy = false,
+    config = function()
+        require("gruvbox").setup({
+            undercurl = false,
+            underline = false,
+            bold = true,
+            italic = {
+                strings = false,
+                comments = false,
+                operators = false,
+                folds = false,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = false,
+            invert_tabline = false,
+            invert_intend_guides = false,
+            inverse = true, -- invert background for search, diffs, statuslinesand errors
+            contrast = "hard",  -- can be "hard", "soft" or empty string
+            overrides = {},
+            vim.cmd("set background=dark")
+        })
+        vim.cmd("colorscheme gruvbox")
+    end,
 }
