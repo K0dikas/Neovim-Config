fd684f908f936ecd0947a22b5cfa177577034691
K0dikas <kodikasYT@protonmail.com>
2023-09-10 15:43:43 +0800

    Added a couple of indendation config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,9 +19,12 @@ end
 
 -- TABS
 vim.opt.autoindent      = true
-vim.opt.tabstop         = 4
+vim.opt.expandtab       = true
+vim.opt.smartindent     = true
+vim.opt.cindent         = true
+vim.opt.tabstop         = 8
 vim.opt.shiftwidth      = 4
-vim.opt.softtabstop     = 4
+-- vim.opt.softtabstop     = 4
 
 -- DISABLE SEMANTIC HIGHLIGHTING
 local links = {
