5482e08e1dfb3dc3593f38270c0faf0d710c1c41
K0dikas <kodikasYT@protonmail.com>
2024-06-28 21:09:38 +0800

    Made html and css lsp server work (finally)

--- lua/plugins/lsp/html.lua
+++ lua/plugins/lsp/html.lua
@@ -1,9 +1,13 @@
 local M = {}
 
+local capabilities = vim.lsp.protocol.make_client_capabilities();
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 function M.config()
     require("lsp_lines").setup{
 
         require'lspconfig'.html.setup{
+            capabilities = capabilities,
             init_options = {
                 configurationSection = { "html", "css", "javascript" },
                 embeddedLanguages = {
@@ -12,7 +16,7 @@ function M.config()
                 },
                 provideFormatter = true
             },
-            filetypes = {"html"},
+            filetypes = {"html", "css"},
             cmd = {"html-languageserver", "--stdio"}
         }
     }
--- lua/plugins/lsp/tailwindcss.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.config()
-    require("lsp_lines").setup{
-        require'lspconfig'.tailwindcss.setup{
-        }
-    }
-end
-return M
